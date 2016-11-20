d1d0b672bf6e27b217f2ed9235e234816ab05fe0
Bishal Pantha <bishal303@gmail.com>
2016-11-20 22:47:09 +0100

    Updated repo info

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import test from 'ava';
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders without crashing', t =>{
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
